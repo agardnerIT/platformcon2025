3d5092a40660f16c22ee5647e3d340798ea95ff3
Adam Gardner <adam.gardner@dynatrace.com>
2025-06-25 11:38:12 +1000

    install curl and start apache

--- environment_installer.py
+++ environment_installer.py
@@ -1,5 +1,6 @@
 import os
 from loguru import logger
+import subprocess
 
 NAME = os.environ.get("NAME", "")
 COLOUR = os.environ.get("COLOUR", "blue")
@@ -13,11 +14,6 @@ with open("/var/www/html/index.html", mode="r") as file:
     # If a line contains a placeholder
     # replace it
     for line in lines:
-        if "PLACEHOLDER" not in line:
-            logger.info("Uninteresting line. Skipping")
-            replaced_lines.append(line)
-            continue
-
         if "NAME_PLACEHOLDER" in line:
             # Must replace NAME_PLACEHOLDER
             line = line.replace("NAME_PLACEHOLDER", NAME)
@@ -29,3 +25,6 @@ with open("/var/www/html/index.html", mode="r") as file:
 
 with open("/var/www/html/index.html", mode="w") as file:
     file.writelines(replaced_lines)
+
+# Start apache2
+subprocess.run(["apache2ctl", "start"])
